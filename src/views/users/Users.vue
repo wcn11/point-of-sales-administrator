2f71c713fb03357187710a6405bd8c0e6fd47477
beliayam <beliayamcom@gmail.com>
2021-09-23 12:25:15 +0700

    v1

--- src/views/users/Users.vue
+++ src/views/users/Users.vue
@@ -8,7 +8,7 @@
 
     <div class="row">
       <div class="col-md-12">
-        <div class="m-auto ">
+        <div class="m-auto">
           <button
             class="btn btn-info mr-1"
             id="btn-update-mass-price"
@@ -21,7 +21,7 @@
             <span class="badge badge-danger">segera</span>
           </button>
           <router-link
-          :to="{name: 'UserAdd'}"
+            :to="{ name: 'UserAdd' }"
             class="btn btn-warning mr-1"
             id="btn-update-mass-price"
             data-toggle="tooltip"
@@ -30,7 +30,9 @@
           >
             <i class="fad fa-usd-circle"></i> tambah mitra
           </router-link>
-          <router-link :to="{ name: 'category' }" class="text-decoration-none btn btn-dark mr-1"
+          <router-link
+            :to="{ name: 'category' }"
+            class="text-decoration-none btn btn-dark mr-1"
             id="btn-update-mass-price"
             data-toggle="tooltip"
             data-placement="bottom"
@@ -38,7 +40,9 @@
           >
             <i class="fad fa-box-usd"></i> Pengaturan Produk
           </router-link>
-          <router-link :to="{ name: 'category' }" class="text-decoration-none btn btn-danger mr-1"
+          <router-link
+            :to="{ name: 'category' }"
+            class="text-decoration-none btn btn-danger mr-1"
             id="btn-update-mass-price"
             data-toggle="tooltip"
             data-placement="bottom"
@@ -130,17 +134,77 @@
               >
                 ubah
               </button>
+              <button
+                @click="deleteUser(user['id'])"
+                type="button"
+                class="btn btn-outline-danger ml-2"
+              >
+                hapus
+              </button>
             </td>
           </tr>
         </tbody>
       </table>
+    </div>
+
+    <!-- Modal -->
+    <div
+      class="modal fade"
+      id="modal-remove-user"
+      tabindex="-1"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header bg-danger text-white">
+            <h5 class="modal-title" id="exampleModalLabel">Hapus Mitra</h5>
+            <button
+              type="button"
+              class="close"
+              data-dismiss="modal"
+              aria-label="Close"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <p>
+              Menghapus mitra sama saja menghapus seluruh data terkait mitra
+              tersebut. <br />
+              Yakin Ingin Menghapusnya ?
+            </p>
+            <small
+              ><i class="fad fa-info-circle"></i> Data pada accurate tidak akan
+              terhapus</small
+            >
+            <br />
+            <small
+              ><i class="fad fa-info-circle"></i> Sekali terhapus, data tidak
+              dapat dikembalikan</small
+            >
+          </div>
+          <div class="modal-footer">
+            <button
+              type="button"
+              class="btn btn-secondary"
+              data-dismiss="modal"
+            >
+              tutup
+            </button>
+            <button type="button" class="btn btn-danger" @click="removeUser()">
+              Ya, saya mengerti
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-// import $ from 'jquery'
+import $ from "jquery";
 
 export default {
   name: "Users",
@@ -148,9 +212,36 @@ export default {
   data() {
     return {
       users: [],
+      selectedUser: 0,
     };
   },
   methods: {
+    deleteUser(id) {
+      this.selectedUser = id;
+      $("#modal-remove-user").modal("show");
+    },
+    removeUser() {
+      return this.$http
+        .delete(
+          `${process.env.VUE_APP_BASE_HOST_API_ADMIN}/user/${this.selectedUser}`,
+          {
+            headers: {
+              Authorization: "Bearer " + localStorage.getItem("jwt-admin"),
+            },
+          }
+        )
+        .then((results) => {
+          if (results["data"]["success"]) {
+            this.$alertify.success("Mitra Berhasil Dihapus");
+            this.getUsers();
+            $("#modal-remove-user").modal("hide");
+          }
+        })
+        .catch((error) => {
+          console.log(error.response);
+          this.$alertify.error(error.response["data"]["message"]);
+        });
+    },
     sync() {
       return this.$http
         .get(`${process.env.VUE_APP_BASE_HOST_API_ADMIN}/sync`, {
@@ -159,7 +250,7 @@ export default {
           },
         })
         .then((results) => {
-          console.log(results)
+          console.log(results);
           // if (results["data"]["success"]) {
           //   this.users = results["data"]["data"];
           // }
